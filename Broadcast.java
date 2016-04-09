45fc4372f70ea415e08f38f280a649af0ec0cad8
ferozen <afff53@gmail.com>
2016-04-09 00:56:10 -0700

    Delete Broadcast.java

--- Broadcast.java
+++
@@ -1,9 +0,0 @@
-package Entertainment;
-
-public interface Broadcast {
-
-	// EntertainmentProject: Amran Feroz, Gong Zhenmu, Michelle Lindblom
-
-	public void addBroadcast(String time, String station);
-	public String[] getListOfBroadcasts();
-}
